01b2980eac6cda30be0489dbdfd500e55b66a42b
Akshay Sakinam <sakinamakshay@gmail.com>
2025-10-02 08:25:15 +0530

    implement auth and storage

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,17 +9,17 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBl6O7BPchbebogJgYElrkOwUUpdvxWEdQ",
+  authDomain: "mockly-a4b1f.firebaseapp.com",
+  projectId: "mockly-a4b1f",
+  storageBucket: "mockly-a4b1f.firebasestorage.app",
+  messagingSenderId: "218308586943",
+  appId: "1:218308586943:web:ec77e13926dedb7d2d450a",
+  measurementId: "G-P9DV1ET06E"
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
