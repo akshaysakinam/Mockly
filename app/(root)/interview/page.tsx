e229331ed90f84d9e94b942d2029435723a78e05
Akshay Sakinam <sakinamakshay@gmail.com>
2025-10-02 08:37:58 +0530

    implement interview generation page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,16 @@
+import Agent from "@/components/Agent";
+// import { getCurrentUser } from "@/lib/actions/auth.action";
+
+const Page = async () => {
+  // const user = await getCurrentUser();
+
+    return (
+        <>
+            <h3>Interview Generation</h3>
+
+            <Agent userName="You" userId="user1" type="generate" />
+        </>
+    )
+};
+
+export default Page;
